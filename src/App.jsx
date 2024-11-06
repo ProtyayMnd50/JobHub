60104c21737d851f8bf1490f00eaa53d994b2438
Protyay Mondal <protyaymondal32@gmail.com>
2024-11-06 20:42:50 +0530

    Defined routes for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import LandingPage from "./pages/LandingPage";
+import OnboardingPage from "./pages/Onboarding";
+import JobPage from "./pages/JobPage";
+import JobListing from "./pages/JobListing";
+import MyJobs from "./pages/MyJobs";
+import PostJob from "./pages/PostJob";
+import SavedJob from "./pages/SavedJob";
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <OnboardingPage />, // Assuming a component named OnboardingPage exists
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/job/:id",
+        element: <JobPage />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/saved-job",
+        element: <SavedJob />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
 const App = () => {
-  return (
-    <div>
-      <Button>hello</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
