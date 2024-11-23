5abcabcce1f3b68f38a057fdc54ec552a5fd92ac
Protyay Mondal <protyaymondal32@gmail.com>
2024-11-23 15:00:39 +0530

    Initial setup for supabase and clerk

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-      <Outlet />
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-zinc-900 mt-10">
+        {" "}
+        made with love by Protyay Mondal
+      </div>
     </div>
   );
 };
