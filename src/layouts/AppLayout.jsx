344c7f2183a134f6857a229944ba950137c7571a
Protyay Mondal <protyaymondal32@gmail.com>
2024-11-23 17:37:54 +0530

    worked on landing page

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -6,10 +6,15 @@ const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-      <main className="min-h-screen container">
+      {/* <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main> */}
+      <main className="min-h-screen w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Header />
         <Outlet />
       </main>
+
       <div className="p-10 text-center bg-zinc-900 mt-10">
         {" "}
         made with love by Protyay Mondal
