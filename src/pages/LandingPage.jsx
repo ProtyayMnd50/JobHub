a4480d662567bdd409dab0bca012b072c5fbe529
Protyay Mondal <protyaymondal32@gmail.com>
2025-02-13 01:14:15 +0530

    changed config file for vite

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -24,15 +24,6 @@ const LandingPage = () => {
       <section className="text-center">
         <h1 className="flex flex-col items-center jusutify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
           Find your dream job here
-          {/* <span className="flex items-center gap-2 sm:gap-6">
-            {" "}
-            and get{" "}
-            <img
-              src="/logo.png"
-              alt="Hired logo"
-              className="h-14 sm:h-24 lg:h-32"
-            ></img>
-          </span> */}
         </h1>
         <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
           Explore thousands of job listings or find the perfect candidate
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   build: {
+//     sourcemap: false, // Disable source map generation in production
+//   },
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path"; // Correct import
+
+// Use import.meta.url to resolve the directory
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  build: {
+    sourcemap: false, // Disable source map generation in production
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Correct alias path
     },
   },
 });
